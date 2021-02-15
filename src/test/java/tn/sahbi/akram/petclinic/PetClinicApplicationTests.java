bc8ffca66f6696dfd7ac6ce31f808f1540455abf
akram.sahbi@esprit.tn <akram.sahbi@esprit.tn>
2021-02-15 20:39:38 +0100

    Initial project Add

---
+++ src/test/java/tn/sahbi/akram/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.sahbi.akram.petclinic;
+
+import org.testng.annotations.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
