6f8a25e248049ff87d0e2a32835003d1df5c121b
akram.sahbi@esprit.tn <akram.sahbi@esprit.tn>
2021-03-06 23:09:59 +0100

    implement map based services. closes #6.

---
+++ pet-clinic-data/src/main/java/tn/sahbi/akram/petclinic/model/services/map/PetMapService.java
@@ -0,0 +1,33 @@
+package tn.sahbi.akram.petclinic.model.services.map;
+
+import tn.sahbi.akram.petclinic.model.Pet;
+import tn.sahbi.akram.petclinic.model.services.CrudService;
+
+import java.util.Set;
+
+public class PetMapService extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+}
---
+++ pet-clinic-data/src/main/java/tn/sahbi/akram/petclinic/model/services/map/VetMapService.java
@@ -0,0 +1,33 @@
+package tn.sahbi.akram.petclinic.model.services.map;
+
+import tn.sahbi.akram.petclinic.model.Vet;
+import tn.sahbi.akram.petclinic.model.services.CrudService;
+
+import java.util.Set;
+
+public class VetMapService extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+}
