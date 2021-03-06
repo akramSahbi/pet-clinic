2cf00e6a3bc7ee3d456253c8cc9f4816c12c8ed6
akram.sahbi@esprit.tn <akram.sahbi@esprit.tn>
2021-03-06 12:38:09 +0100

    adding Id for entities. Closes #10.

--- pet-clinic-data/src/main/java/tn/sahbi/akram/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/tn/sahbi/akram/petclinic/model/Person.java
@@ -1,6 +1,6 @@
 package tn.sahbi.akram.petclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/tn/sahbi/akram/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/tn/sahbi/akram/petclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package tn.sahbi.akram.petclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
     private String name;
 
     public String getName() {
