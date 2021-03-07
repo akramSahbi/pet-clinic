c2fc0146836f5ed5a706eeba5f31d267aa6eff1e
akram.sahbi@esprit.tn <akram.sahbi@esprit.tn>
2021-03-07 10:21:26 +0100

    create owner index page and controller. closes #13

---
+++ pet-clinic-web/src/main/java/tn/sahbi/akram/petclinic/controllers/OwnerController.java
@@ -0,0 +1,14 @@
+package tn.sahbi.akram.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/owners")
+public class OwnerController {
+
+    @RequestMapping({"", "index", "index.html"})
+    String listOwners() {
+        return "owners/index";
+    }
+}
