6e8722bb0976bae4205881cdd2a3593485abe0ac
akram.sahbi@esprit.tn <akram.sahbi@esprit.tn>
2021-03-12 12:58:17 +0100

    list all owners on owner index page. closes #16

--- pet-clinic-web/src/main/java/tn/sahbi/akram/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/tn/sahbi/akram/petclinic/controllers/OwnerController.java
@@ -1,14 +1,23 @@
 package tn.sahbi.akram.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import tn.sahbi.akram.petclinic.model.services.OwnerService;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
 
     @RequestMapping({"", "index", "index.html"})
-    String listOwners() {
+    String listOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
