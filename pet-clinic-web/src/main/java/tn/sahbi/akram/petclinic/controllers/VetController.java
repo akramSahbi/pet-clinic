e863a98f4e98ac37dad24b0dd6eefe952a3a0948
akram.sahbi@esprit.tn <akram.sahbi@esprit.tn>
2021-03-12 13:03:43 +0100

    list all vets on vet index page. closes #15

--- pet-clinic-web/src/main/java/tn/sahbi/akram/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/tn/sahbi/akram/petclinic/controllers/VetController.java
@@ -1,13 +1,22 @@
 package tn.sahbi.akram.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import tn.sahbi.akram.petclinic.model.services.VetService;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets","/vets/index", "/vets/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
