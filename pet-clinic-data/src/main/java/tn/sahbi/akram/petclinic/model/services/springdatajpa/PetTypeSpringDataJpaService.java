636973b66c75d947258ae5ac138b550eca9c3593
akram.sahbi@esprit.tn <akram.sahbi@esprit.tn>
2021-05-01 23:27:40 +0100

    create spring data jpa service for pet type.closes #43

--- pet-clinic-data/src/main/java/tn/sahbi/akram/petclinic/model/services/springdatajpa/PetTypeSpringDataJpaService.java
+++ pet-clinic-data/src/main/java/tn/sahbi/akram/petclinic/model/services/springdatajpa/PetTypeSpringDataJpaService.java
@@ -1,5 +1,7 @@
 package tn.sahbi.akram.petclinic.model.services.springdatajpa;
 
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 import tn.sahbi.akram.petclinic.model.PetType;
 import tn.sahbi.akram.petclinic.model.repositories.PetTypeRepository;
 import tn.sahbi.akram.petclinic.model.services.PetTypeService;
@@ -7,6 +9,8 @@ import tn.sahbi.akram.petclinic.model.services.PetTypeService;
 import java.util.HashSet;
 import java.util.Set;
 
+@Service
+@Profile("springdatajpa")
 public class PetTypeSpringDataJpaService implements PetTypeService {
     private final PetTypeRepository petTypeRepository;
 
