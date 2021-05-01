a1ebd2524d051e7872262327bfec00f42a4318b4
akram.sahbi@esprit.tn <akram.sahbi@esprit.tn>
2021-05-01 19:59:44 +0100

    create visit entity.closes #39

--- pet-clinic-data/src/main/java/tn/sahbi/akram/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/tn/sahbi/akram/petclinic/model/Pet.java
@@ -1,9 +1,9 @@
 package tn.sahbi.akram.petclinic.model;
 
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Pet extends BaseEntity {
@@ -14,6 +14,9 @@ public class Pet extends BaseEntity {
     @ManyToOne
     @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
     private LocalDate birthdate;
 
     public PetType getPetType() {
